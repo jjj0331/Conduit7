04c26a78e10eb8627c33e54d00786ef1e7e01e3c
jjj0331 <19980331jj@gmail.com>
2024-04-20 20:29:01 +0900

    rails generate controller Sessions new

--- app/controllers/conduit_controller.rb
+++ app/controllers/conduit_controller.rb
@@ -3,5 +3,19 @@ class ConduitController < ApplicationController
   end
 
   def register
-  end 
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'home'
+    end   
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'conduit#home'
 
   get 'conduit/register', to:'conduit#register', as: :register
+  post 'conduit/create', to:'conduit#create'    , as: :create
   
 end
